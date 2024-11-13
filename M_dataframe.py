0d61cb88076005ba4185a3ff602ea674ea15217c
Mina-T <taleblou.mina@gmail.com>
2024-11-13 17:16:35 +0100

    changed

--- M_dataframe.py
+++ M_dataframe.py
@@ -4,19 +4,21 @@ import os
 import pickle
 import matplotlib.pyplot as plt
 
+
 def read_metrics(path):
-    '''
-    Given a path, converts the metrics.dat into pd.df
-    '''
-    df= pd.read_csv(path + '/metrics.dat', delim_whitespace = True )
-    df = df.groupby(['#epoch'], as_index=False).last()
+    """
+    Given a path, converts the metrics.dat file into a pandas DataFrame.
+    """
+    df = pd.read_csv(f"{path}/metrics.dat", delim_whitespace=True, usecols=['#epoch'])
+    df = df.groupby('#epoch', as_index=False).last()
     return df
 
-def read_dat(path, _file):
+
+
+def read_dat(path, file):
     '''
-    Given a path, converts the *.dat into pd.df
+    Given a path, converts the dat into df
     '''
-    if path[-1] != '/':
-        path = path + '/'
-    df= pd.read_csv(path + _file, delim_whitespace = True )
+    _file = os.path.join(path, file)
+    df= pd.read_csv(_file, delim_whitespace = True )
     return df
